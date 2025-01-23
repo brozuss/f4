ab5d669fccd1782717565b0c25adb79e1c7f1e5b
Makovskyyy <plackowaty724@gmail.com>
2025-01-23 18:14:40 +0100

    Dodałem funkcję która rysuje linie siatki na planszy przed rozpoczęciem symulacji. Linie dzielą planszę na komórki określonej wielkości przez zmienną "rozmiar_komorki".

--- maks.py
+++ maks.py
@@ -7,18 +7,18 @@ Program tworzy symulację "Mrówki Langtona" za pomocą biblioteki turtle.
 Symulacja polega na tym, że mrówka porusza się po siatce (planszy) zgodnie z 3 regułami:
 1. Jeżeli znajduje się w komórce żywej, to skręca w prawo o 90 stopni.
 2. Jeżeli znajduje się w komórce martwej, to skręca w lewo o 90 stopni.
-3. Przechodząc do następnego pola na siatce zmienia jego stan z żywego na martwy lub odwrotnie
+3. Przechodząc do następnego pola na siatce zmienia jego stan z żywego na martwy lub odwrotnie.
 
 Dzięki bibliotece time możemy operować czasem np. opóźnieniem kroku.
 
 Parametry możemy edytować wedle naszego uznania:
-x- Szerokość planszy
-y- Wysokość planszy
-x_0- Położenie początkowe w poziomie
-y_0- Położenie początkowe w pionie
-kroki- Liczba iteracji do wykonania przez mrówkę
-rozmiar_komorki- Rozmiar pojedynczej komórki
-kierunki-  Kierunki ruchu. Odpowiadają za zmiane współrzędnych mrówki (przesunięcie o daną ilość komórek w daną stronę)
+x- Szerokość planszy.
+y- Wysokość planszy.
+x_0- Położenie początkowe w poziomie.
+y_0- Położenie początkowe w pionie.
+kroki- Liczba iteracji do wykonania przez mrówkę.
+rozmiar_komorki- Rozmiar pojedynczej komórki.
+kierunki-  Kierunki ruchu. Odpowiadają za zmiane współrzędnych mrówki (przesunięcie o daną ilość komórek w daną stronę).
 
 Funkcja "rysuj_komorke":
 Rysuje pojedynczą komórkę na planszy, przyjmując jako argumenty współrzędne i kolor.
@@ -39,18 +39,22 @@ Zmienia kolor aktualnej komórki na przeciwny.
 Przesuwa mrówkę o jeden krok w nowym kierunku.
 Aktualizuje wyświetlany obraz.
 
+Funkcja "rysuj_siatke":
+Odpowiada za rysowanie linii siatki na planszy w wybranym kolorze przed rozpoczęciem symulacji.
+Linie dzielą planszę na komórki określonej wielkości przez zmienną "rozmiar_komorki".
+
 Wywołanie "symulacja()" odpowiada za wyświetlanie animacji.
 
 """
 
 # Parametry
-x= 100
-y= 100
-x_0= 50
-y_0= 50
-kroki= 100000
-rozmiar_komorki= 10
-kierunki= [(0,1), (1,0), (0,-1), (-1,0)]
+x = 100
+y = 100
+x_0 = 50
+y_0 = 50
+kroki = 100000
+rozmiar_komorki = 10
+kierunki = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 # Rysunek
 def rysuj_komorke(x_pozycja, y_pozycja, kolor):
@@ -59,36 +63,53 @@ def rysuj_komorke(x_pozycja, y_pozycja, kolor):
     turtle.pendown()
     turtle.fillcolor("black" if kolor else "white")
     turtle.begin_fill()
-    for a in range(4): # Pętla rysująca komórkę
+    for a in range(4):  # Pętla rysująca komórkę
         turtle.forward(1)
         turtle.left(90)
     turtle.end_fill()
 
 # Krok mrówki
-def krok(siatka, mrowka_x, mrowka_y, kierunek_mrowki, x, y): # Wykonuje pojedynczy krok symulacji, zmienia kolor komórki oraz odpowiada za ruch mrówki
+def krok(siatka, mrowka_x, mrowka_y, kierunek_mrowki, x, y):  # Wykonuje pojedynczy krok symulacji, zmienia kolor komórki oraz odpowiada za ruch mrówki
     obecna_komorka = siatka[mrowka_y][mrowka_x]
     kierunek_mrowki = (kierunek_mrowki - 1 if obecna_komorka == 0 else kierunek_mrowki + 1) % 4
     siatka[mrowka_y][mrowka_x] = 1 - obecna_komorka
     rysuj_komorke(mrowka_x, mrowka_y, siatka[mrowka_y][mrowka_x])
     dx, dy = kierunki[kierunek_mrowki]
-    return siatka, (mrowka_x + dx) % x, (mrowka_y + dy) % y, kierunek_mrowki # Zwraca zaaktualizowaną siatkę, nową pozycję i nowy kierunek mrówki
+    return siatka, (mrowka_x + dx) % x, (mrowka_y + dy) % y, kierunek_mrowki  # Zwraca zaaktualizowaną siatkę, nową pozycję i nowy kierunek mrówki
 
 # Uruchamianie symulacji
-def symulacja(): # Uruchamia symulację
+def symulacja():  # Uruchamia symulację
     siatka = [[0] * x for a in range(y)]
-    mrowka_x = x_0 # Położenie mrówki w poziomie
-    mrowka_y = y_0 # Położenie mrówki w pionie
+    mrowka_x = x_0  # Położenie mrówki w poziomie
+    mrowka_y = y_0  # Położenie mrówki w pionie
     kierunek_mrowki = 0
-    for a in range(kroki): # Pętla wykonuje zadaną przez nas ilość iteracji
+    for a in range(kroki):  # Pętla wykonuje zadaną przez nas ilość iteracji
         siatka, mrowka_x, mrowka_y, kierunek_mrowki = krok(siatka, mrowka_x, mrowka_y, kierunek_mrowki, x, y)
         turtle.update()
-        time.sleep(0.01) # Czas opóźnienia każdej iteracji
+        time.sleep(0.01)  # Czas opóźnienia każdej iteracji
+
+# Linie na planszy
+def rysuj_siatke(x, y):
+    turtle.color("grey")
+    turtle.penup()
+    for i in range(x + 1):
+        turtle.goto(i, 0)
+        turtle.pendown()
+        turtle.goto(i, y)
+        turtle.penup()
+    for i in range(y + 1):
+        turtle.goto(0, i)
+        turtle.pendown()
+        turtle.goto(x, i)
+        turtle.penup()
 
 turtle.setup(x * rozmiar_komorki, y * rozmiar_komorki)
 turtle.setworldcoordinates(0, 0, x, y)
 turtle.speed(0)
 turtle.hideturtle()
-turtle.tracer(0,0)
+turtle.tracer(0, 0)
+
+rysuj_siatke(x, y)  # Rysowanie siatki przed rozpoczęciem symulacji
 
 symulacja()
 turtle.done()
